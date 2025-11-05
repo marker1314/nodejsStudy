a0ec2caa126a1bb5768927a2efc15837a7baacac
tjsn-00 <qor0628@korea.ac.kr>
2025-11-06 00:04:37 +0900

    Day 02 - Routing
    5~10강 수강
    기초 문법을 마무리하고 모듈을 배우고 HTTP 모듈로 서버를 만들고 라우팅함

--- basics/04/server-5.js
+++ basics/04/server-5.js
@@ -1,1 +1,28 @@
 // 라우팅 연습하기  (결과 비교 파일 : 04\results\server-5.js)
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    // req.url : 요청 경로
+    // req.method : 요청 방식
+
+    const { url, method } = req;
+    // const url = req.url;
+    // const method = req.method;
+
+    res.setHeader("Content-type", "text/plain");
+
+    if (method === "GET" && url === "/home"){
+        res.write("Home");
+        res.end();
+    } else if (method === "GET" && url === "/about"){
+        res.end("About");
+    } else {
+        res.end("Not Found");
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log("서버 실행 중");
+});
