a7da085f98206c04bdf14b25e9fb9cd5024bfb2d
tjsn-00 <qor0628@korea.ac.kr>
2025-11-20 14:22:34 +0900

    Day 04 - userVerify
    26강 사용자 인증하기

--- myContacts/controllers/loginController.js
+++ myContacts/controllers/loginController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
 
 // Get login page
 // GET /
@@ -12,12 +15,19 @@ const getLogin = asyncHandler(async (req, res) => {
 // POST /
 const loginUser = asyncHandler(async (req, res) => {
     const { username, password } = req.body;
-
-    if (username === "admin" && password === "1234") {
-        res.send("Login success");
-    }else{
-        res.send("Login failed");
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.json({ message: "일치하는 사용자가 없습니다." });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.json({ message: "비밀번호가 일치하지 않습니다." });
+    }
+
+    const token = jwt.sign({ id: user._id}, jwtSecret);
+    res.cookie("token", token, { httpOnly: true});
+    res.redirect("/contacts");
 });
 
 // Register page
