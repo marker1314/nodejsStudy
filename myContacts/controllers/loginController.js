93821c50b09fc8a5709a12e280c7b2eb3e6e2e22
FIo-g <xogud0710@korea.ac.kr>
2025-11-20 11:53:08 +0900

    Day 08 - admin, cerification
    24,25강 - 관리자 등록하기, 사용자 인증하기

--- myContacts/controllers/loginController.js
+++ myContacts/controllers/loginController.js
@@ -1,4 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+
+
 
 //Get login Page
 //GEt/
@@ -10,13 +17,38 @@ const getLogin = (req,res)=>{
 //POST /
 const loginUser = asyncHandler(async(req,res)=>{
     const {username, password} = req.body;
+    
+    const user = await User.findOne({username});
+    if(!user){
+        return res.json({message: "일치하는 사용자가 없습니다. "});
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        return res.json({message: "비밀번호가 맞지 않습니다. "});
+    }
 
-    if(username ==="admin" && password==="1234"){
-        res.send("login Success");
-    }
-    else{
-        res.send("login Failed");
-    }
+    const token = jwt.sign({id: user._id}, jwtSecret);     
+    res.cookie("token",token,{httpOnly: true});
+    res.redirect("/contacts")
 });
 
-module.exports = {getLogin , loginUser};
+//Register page
+//GET/register
+const getRegister =(req,res) =>{
+    res.render("register");
+}
+
+//Register User
+//POST/register
+const registerUser = asyncHandler(async(req,res)=>{
+    const{username, password, password2} =req.body;
+    if (password === password2){ 
+        const hashedPassword = await bcrypt.hash(password,10); //비밀번호 해시화 
+        const user = await User.create({username, password:hashedPassword}); //스키마 생성
+        res.json({message:"Register successful", user});
+
+    }else{
+        res.send("register failed");
+    }
+})
+module.exports = {getLogin , loginUser , getRegister, registerUser};
